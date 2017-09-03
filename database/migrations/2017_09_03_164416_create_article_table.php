0060c8624c0848be5f444d541649b0956df414b7
chichoyi <cys843755799@163.com>
2017-09-04 00:53:19 +0800

    添加数据迁移

---
+++ database/migrations/2017_09_03_164416_create_article_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            //title,cover_url,description,tags_id,content,is_original,timestamps,u_id,status
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
---
+++ database/migrations/2017_09_03_164529_create_article_comment_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_comment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            //a_id,u_id,comment,status,create_time
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_comment');
+    }
+}
